49bf7cde0d7ddbcea4a47bc51e323559b70cec71
jcherwin <117125528+jcherwin@users.noreply.github.com>
2025-11-09 00:55:15 -0600

    Update Bio on About page

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -5,11 +5,11 @@ export default function About() {
   return (
     <div className='d-flex flex-row flex-wrap'>
       <h2 className='p-2 w-100'>About Me</h2>
-      <img src='./img/headshot.jpg' alt='headshot' className='p-2 headshot align-self-start' />
+      <img src='/img/headshot.jpg' alt='headshot' className='p-2 headshot align-self-start' />
       <p className='fs-4 about'>
-        My name is Jon Cherwin. <br/><br/> 
-        I’m a Full-Stack Web Developer with over 7 years of technical experience turning ideas into fast, scalable, and visually engaging digital products. <br/><br/>
-        Always evolving, I stay at the forefront of new technologies to deliver results that drive growth and innovation.
+        Hello, my name is Jon Cherwin. <br/><br/> 
+        I’m a Full-Stack Web Developer with over 7 years of technical experience of turning ideas into fast, scalable, and visually engaging digital products. <br/><br/>
+        Always learning, I make sure to stay at the forefront of new technologies in order to deliver results that drive growth and innovation.
       </p>
     </div>
   );
